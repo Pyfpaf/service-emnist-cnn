0bfbeba4e1adca0f10ecaba20c2adb88cc7b4707
Alex <bocandzo@mail.ru>
2024-10-11 20:09:33 +0300

    Completed model

--- myapp/main.py
+++ myapp/main.py
@@ -1,8 +1,10 @@
 import torch
+import numpy as np
 
 from fastapi import FastAPI, Body
 from fastapi.staticfiles import StaticFiles
 from myapp.model import Model
+from torchvision.transforms import ToTensor, Compose, Normalize
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -11,16 +13,30 @@ logger = logging.getLogger(__name__)
 
 # load model
 model = Model()
+model.load_state_dict(torch.load('myapp/model.ckpt'))
+
+# app
+with open('emnist-balanced-mapping.txt', 'r') as file:
+    content = file.read()
+lines = content.strip().split('\n')
+labels_dict = {int(lines[i].split(' ')[0]): chr(int(lines[i].split(' ')[1])) for i in range(len(lines))}
 
 # app
 app = FastAPI(title='Symbol detection', docs_url='/docs')
 
+transform = Compose([
+    Normalize([0.5], [0.5])
+])
+
 # api
 @app.post('/api/predict')
 def predict(image: str = Body(..., description='image pixels list')):
-    image = torch.tensor(list(map(int, image[1:-1].split(',')))).reshape((28, 28))
-    pred = model.predict(image)
-    return {'prediction': pred}
+    image = torch.tensor(list(map(float, image[1:-1].split(',')))).reshape((28, 28)).transpose(1, 0).unsqueeze(0).unsqueeze(0)
+    x = transform(image)
+    model.eval()
+    with torch.no_grad():
+        pred = model(x)
+    return {'prediction': labels_dict[pred.argmax().item()]}
 
 # static files
 app.mount('/', StaticFiles(directory='static', html=True), name='static')
--- myapp/model.py
+++ myapp/model.py
@@ -1,1 +1,26 @@
-# your code here
+import torch.nn as nn
+
+
+class Model(nn.Module):
+    def __init__(self, n_classes=47):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 32, (3, 3), padding=1),  # (batch_size, 1, 28, 28) -> (batch_size, 32, 28, 28)
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2),  # (batch_size, 1, 28, 28) -> (batch_size, 32, 14, 14)
+            nn.Conv2d(32, 64, (3, 3)),  # (batch_size, 32, 14, 14) -> (batch_size, 64, 12, 12)
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),  # (batch_size, 64, 12, 12) -> (batch_size, 64, 6, 6)
+            nn.Flatten(),  # (batch_size, 64, 6, 6) -> (batch_size, 64*6*6)
+            nn.Linear(64 * 6 * 6, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, 128),
+            nn.ReLU(),
+            nn.Linear(128, n_classes)
+        )
+
+    def forward(self, x):
+        return self.model(x)
